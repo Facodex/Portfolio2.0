b8a324f4284e4200552290bf9a11ff7d66c0f98b
Facundo Benitez <facu.cr9@gmail.com>
2022-11-20 08:55:13 -0300

    Section Experience Ready

---
+++ src/components/Experience.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export const Experience = () => {
+  return (
+    <section className='experienceContainer'>
+
+        <h1 className='titlesSections'>Experiencie</h1>
+
+        <div className='circleContainer'>
+            <div className="circle"></div>
+        </div>
+
+        <div className='experienceRight'>
+            <div className='experienceItemRight'>
+                <h1 className='titleExperience'>FRONTEND DEVELOPER</h1>
+                <p className='pExperience'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Consectetur accusantium ad non eum delectus nostrum iure distinctio maxime? Unde mollitia
+                    aut sequi. Recusandae iste reiciendis earum sed officia enim labore.
+                </p>
+            </div>
+        </div>
+
+        <div className='circleContainer'>
+            <div className="circleSimple"></div>
+        </div>
+
+        <div className='.experienceLeft'>
+            <div className='experienceItemLeft'>
+                <h1 className='titleExperience'>FRONTEND DEVELOPER</h1>
+                <p className='pExperience'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Consectetur accusantium ad non eum delectus nostrum iure distinctio maxime? Unde mollitia
+                    aut sequi. Recusandae iste reiciendis earum sed officia enim labore.
+                </p>
+            </div>
+        </div>
+
+
+    </section>
+  )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { About } from './About'
+import { Experience } from './Experience'
 import { FrontPage } from './FrontPage'
 import { ScrollDown } from './ScrollDown'
 import { Skills } from './Skills'
@@ -14,6 +15,10 @@ export const Main = () => {
         <About/>
 
         <Skills/>
+
+        <Experience/>
+
+        <footer style={{width:'100%', height:'100px', backgroundColor:'white', marginTop:'20px',}}></footer>
     </div>
   )
 }
