121ebefb658bb2d51fe980994c96a1c7daad4ea3
Facundo Benitez <facu.cr9@gmail.com>
2022-11-25 11:04:59 -0300

    project card style ready

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from 'react';
+import appPelis from '../assets/images/img-app-pelis.png';
+import github from '../assets/icons/github-icon.png';
 
 export const Projects = () => {
   return (
     <section id='projects' className='projectsSection'>
-        <h1 className='titlesSections'>Projects</h1>
-        <div className='containerCardsProjects'>
-            <div className='itemProject'>
-                <h3>React Blog</h3>
-            </div>
-            <div className='itemProject'></div>
-            <div className='itemProject'></div>
-            <div className='itemProject'></div>
-            <div className='itemProject'></div>
+      <h1 className='titlesSections'>Projects</h1>
+      <div className='containerCardsProjects'>
+
+
+        <div className='itemProject'>
+          <img src={appPelis} className='imgProject' />
+          <h3>Aplicacion de peliculas</h3>
+          <p> HTML - CSS - ReactJS </p>  
+          <a href="/#">Ver detalle</a>
         </div>
+
+
+        <div className='itemProject'></div>
+        <div className='itemProject'></div>
+        <div className='itemProject'></div>
+        <div className='itemProject'></div>
+      </div>
 
     </section>
   )
